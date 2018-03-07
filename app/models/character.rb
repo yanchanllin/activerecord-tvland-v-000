4a078c87c1b84cbd434e9e1e4d086714edda0b14
YanC Lin <yonalin1@gmail.com>
2018-03-07 23:28:06 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,7 +5,7 @@ class Character < ActiveRecord::Base
    def say_that_thing_you_say
      "#{self.name} always says: #{self.catchphrase}"
    end
-   def build_network 
-     
-   end 
+   def build_network
+
+   end
 end
